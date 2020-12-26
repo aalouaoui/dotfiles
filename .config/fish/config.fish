45888432e325ecee250dac7edd3bec9d171ef3af
aalouaoui <khalidaalouaoui@gmail.com>
2020-12-26 20:38:59 +0100

    Fish Aliases

---
+++ .config/fish/config.fish
@@ -0,0 +1,8 @@
+alias ls="lsd"
+alias bat="bat -P"
+alias grep='grep --color=auto'
+alias fgrep='fgrep --color=auto'
+alias egrep='egrep --color=auto'
+alias diff='diff --color=auto'
+alias colorscript="/opt/shell-color-scripts/colorscript.sh"
+alias xephyr="Xephyr -br -ac -noreset -screen 1360x700 :1 &"
--- .zshrc
+++ .zshrc
@@ -111,7 +111,7 @@ alias ll="lsd -lah --group-dirs first"
 alias la="lsd -ah --group-dirs first"
 alias tree='lsd -ah --tree'
 alias config="micro ~/.zshrc"
-alias bat="PAGER= bat"
+alias bat="bat -P"
 alias grep='grep --color=auto'
 alias fgrep='fgrep --color=auto'
 alias egrep='egrep --color=auto'
