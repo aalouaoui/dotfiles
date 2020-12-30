0c5d9fcfe3f8df66ea44f03d91c6b1bbc8bb83e4
aalouaoui <khalidaalouaoui@gmail.com>
2020-12-30 23:52:06 +0100

    Used Fish abbr instead of aliases

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,8 +1,10 @@
-alias ls="lsd"
-alias bat="bat -P"
-alias grep='grep --color=auto'
-alias fgrep='fgrep --color=auto'
-alias egrep='egrep --color=auto'
-alias diff='diff --color=auto'
-alias colorscript="/opt/shell-color-scripts/colorscript.sh"
-alias xephyr="Xephyr -br -ac -noreset -screen 1360x700 :1 &"
+abbr --add --global ls "lsd"
+abbr --add --global la "lsd -a"
+abbr --add --global ll "lsd -la"
+abbr --add --global bat "bat -P"
+abbr --add --global grep 'grep --color=auto'
+abbr --add --global fgrep 'fgrep --color=auto'
+abbr --add --global egrep 'egrep --color=auto'
+abbr --add --global diff 'diff --color=auto'
+abbr --add --global colorscript "/opt/shell-color-scripts/colorscript.sh"
+abbr --add --global xephyr "Xephyr -br -ac -noreset -screen 1360x700 :1 &"
